1d647e1431ad151dbb08d984254a71b5bfa6e690
George <killyen444@gmail.com>
2024-04-23 23:20:46 -0700

    Merge branch 'jpbm135' into 'george'

--- src/utils/send-response.php
+++ src/utils/send-response.php
@@ -11,12 +11,14 @@ function send_error_response($message, $status_code, $die = true)
   }
 }
 
+// Send a JSON response with the provided data
 function send_response($data, $status_code = 200)
 {
   header('Content-Type: application/json');
   http_response_code($status_code);
   echo json_encode($data);
 }
+
 
 function send_non_data_response()
 {
