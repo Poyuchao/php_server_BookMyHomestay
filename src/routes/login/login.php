4e4f2834361ec7eecb8f6de2138edd287d5614a1
JPBM135 <jpedrobm0@gmail.com>
2024-05-02 20:27:24 -0700

    fix(login): fix php tag

--- src/routes/login/login.php
+++ src/routes/login/login.php
@@ -1,19 +1,17 @@
-<?
+<?php
 require_once 'utils/send-response.php';
 require_once 'structures/Route.php';
 
-$loginRoute= (new RouteBuilder())
+$loginRoute = (new RouteBuilder())
     ->setMethod('POST')
     ->setPath('/login')
     ->setHandler(function ($params) {
-        
     })
     ->build();
 
 
 
-function login($UserEmail){
-    $sql = "SELECT * FROM users WHERE email = '$UserEmail' ;" ;
-    
+function login($UserEmail)
+{
+    $sql = "SELECT * FROM users WHERE email = '$UserEmail' ;";
 }
-
